0d7a4b82c381f3c1171e88215f793e1872a16766
CDinuwan <chanukadinuwan35@gmail.com>
2021-04-02 20:02:22 +0530

    First

--- first.js
+++ first.js
@@ -1,3 +1,15 @@
 var number=5; //This is single line comment
 
 /* This is a multiline-comment */
+
+/*Data types:
+undefined,null,boolean,string,symbol,number and object
+*/
+var myName="Chanuka";
+myName=8;
+
+let ourName="Free Code Camp";
+
+const pi=3.14;
+
+console.log("Chanuka");
